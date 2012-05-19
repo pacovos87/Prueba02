c9c31cf9a7a2b6d9c06881096a347c428d54df9b
Sobremesa <Sobremesa@Sobremesa-PC>
2012-05-19 21:47:24 +0200

    Modificacion 1º Test1 y Test2

--- src/git/GitTest2.java
+++ src/git/GitTest2.java
@@ -6,8 +6,7 @@ public class GitTest2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Modificacion1º");
+		System.out.println("Modificacion 1");
 
 	}
 
