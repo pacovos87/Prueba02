6f8ad8f0d486b8f25016803f735ac77b2d6799a3
Sobremesa <Sobremesa@Sobremesa-PC>
2012-05-19 22:08:17 +0200

    Modificacion2º Test1

--- src/git/GitTest1.java
+++ src/git/GitTest1.java
@@ -7,6 +7,11 @@ public class GitTest1 {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Modificacion 1");
+		
+		System.out.println("Modificacion 2");
+		System.out.println("Modificacion 2");
+		System.out.println("Modificacion 2");
+		
 
 	}
 
